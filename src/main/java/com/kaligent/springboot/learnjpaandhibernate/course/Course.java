768cdc8cfa8aebc9a2ed7d65d264f516d4a1a521
Daniel Pérez <ing.dany@gmail.com>
2023-06-30 16:15:13 -0700

    Create JPA Class

--- src/main/java/com/kaligent/springboot/learnjpaandhibernate/course/Course.java
+++ src/main/java/com/kaligent/springboot/learnjpaandhibernate/course/Course.java
@@ -1,8 +1,16 @@
 package com.kaligent.springboot.learnjpaandhibernate.course;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+
+    @Id
     private long id;
+
     private String name;
+
     private String author;
 
     public Course() {
--- src/main/java/com/kaligent/springboot/learnjpaandhibernate/course/jdbc/CourseCommandLineRunner.java
+++ src/main/java/com/kaligent/springboot/learnjpaandhibernate/course/jdbc/CourseCommandLineRunner.java
@@ -1,23 +1,28 @@
 package com.kaligent.springboot.learnjpaandhibernate.course.jdbc;
 
 import com.kaligent.springboot.learnjpaandhibernate.course.Course;
+import com.kaligent.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 
+
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+public class CourseCommandLineRunner implements CommandLineRunner{
+
+    // @Autowired
+    // private CourseJdbcRepository repository;
 
     @Autowired
-    private CourseJdbcRepository repository;
+    private CourseJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
         // TODO Auto-generated method stub
-        repository.insert(new Course(1, "Learn AWS", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure", "in28minutes"));
-        repository.insert(new Course(3, "Learn GCP", "in28minutes"));
+        repository.insert(new Course(1, "Learn AWS JPA", "in28minutes"));
+        repository.insert(new Course(2, "Learn Azure JPA", "in28minutes"));
+        repository.insert(new Course(3, "Learn GCP JPA", "in28minutes"));
 
         repository.deleteById(1);
         System.out.println(repository.findById(2));
